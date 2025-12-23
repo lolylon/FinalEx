97a438012dc8777a55f52934d798a739b6a5d4d0
= <=>
2025-12-23 15:48:03 +0500

    Add Home Controller

--- src/main/java/com/neckfurry/finalexam/controller/HomeController.java
+++ src/main/java/com/neckfurry/finalexam/controller/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello world!";
+        return "Hello world!!!";
     }
 
     @GetMapping("/info")
