e9cd30a495c3344e58b140ef758e8e5a5446f694
= <=>
2025-12-23 15:30:34 +0500

    Init Spring Boot base structure

--- src/main/java/com/neckfurry/finalexam/FinalExamApplication.java
+++ src/main/java/com/neckfurry/finalexam/FinalExamApplication.java
@@ -3,7 +3,6 @@ package com.neckfurry.finalexam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class FinalExamApplication {
 
